cf231df64e28d394c8f0bcbffc77a1528b0cf85e
TheLocust3 <jake.kinsella@gmail.com>
2017-08-06 14:58:36 -0400

    Install material components web and create button component

---
+++ src/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export class Button extends React.Component {
+
+    render() {
+        return (
+            <div>
+                Button
+            </div>
+        );
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { Button } from './Button.jsx'
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <Button />,
   document.getElementById('root')
 );
