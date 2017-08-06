e3a36da2fc7300bcc0e6352001d2b2f10b91d61e
TheLocust3 <jake.kinsella@gmail.com>
2017-08-06 17:33:36 -0400

    Create basic button class

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,12 +1,38 @@
+import _ from 'lodash';
 import React from 'react';
+import PropTypes from 'prop-types';
+
+const VARIANTS = { flat: '', raised: 'mdc-button--raised' }
+const COLORS = { default: '', primary: 'mdc-button--primary', accent: 'mdc-button--accent' }
+const SIZE = { default: '', compact: 'mdc-button--compact', dense: 'mdc-button--dense', small: 'mdc-button--compact mdc-button--dense' }
 
 export class Button extends React.Component {
 
+    constructor(props) {
+        super(props)
+    }
+
     render() {
+        const { color, size, variant, disabled, ...reactProps } = this.props;
+
         return (
-            <div>
-                Button
-            </div>
+            <button className={`mdc-button ${VARIANTS[variant]} ${COLORS[color]} ${SIZE[size]}`} disabled={disabled ? 'disabled' : ''}>
+              {this.props.children}
+            </button>
         );
     }
+}
+
+Button.propTypes = {
+    variant: PropTypes.string,
+    color: PropTypes.string,
+    size: PropTypes.string,
+    disabled: PropTypes.bool,
+}
+
+Button.defaultProps = {
+    variant: 'flat',
+    color: 'default',
+    size: 'default',
+    disabled: false
 }
---
+++ test/test-button.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+
+describe('Button', () => {
+  describe('flat', () => {
+    it('should render', () => {
+      assert.equal(1, 1)
+    });
+  });
+});
