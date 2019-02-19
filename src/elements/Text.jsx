4c20c20bb96eae729fc274adc7402217eff83b11
TheLocust3 <jake.kinsella@gmail.com>
2019-02-19 17:47:04 -0500

    Add ability to use span instead of divs in Text

--- src/elements/Text.jsx
+++ src/elements/Text.jsx
@@ -4,10 +4,18 @@ import PropTypes from 'prop-types';
 
 class Text extends React.Component {
   render() {
-    let { className, outerClassName, type, children, ...props } = this.props;
+    let { className, outerClassName, type, children, span, ...props } = this.props;
 
     className = _.isEmpty(className) ? '' : className;
     outerClassName = _.isEmpty(outerClassName) ? '' : outerClassName;
+
+    if (span) {
+      return (
+        <span className={`mdc-typography mdc-typography--${type} ${outerClassName}`} {...props}>
+          <div className={className}>{children}</div>
+        </span>
+      );
+    }
 
     return (
       <div className={`mdc-typography mdc-typography--${type} ${outerClassName}`} {...props}>
@@ -21,7 +29,8 @@ Text.propTypes = {
   className: PropTypes.string,
   outerClassName: PropTypes.string,
   type: PropTypes.string.isRequired,
-  children: PropTypes.any
+  children: PropTypes.any,
+  span: PropTypes.bool
 };
 
 export default Text;
